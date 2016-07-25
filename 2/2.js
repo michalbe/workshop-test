e016dacaf4d3146451b6e797f588ca74b7c70487
Michał Budzyński <michal@virtualdesign.pl>
2016-07-25 13:45:00 +0100

    :woman: 2nd test

---
+++ 2/2.js
@@ -0,0 +1,21 @@
+'use strict';
+var assert = require('assert');
+
+// LOGIC
+var getNewUserInstance = function(a, b) {
+  return {
+    firstName: 'Pol',
+    secondName: 'Pot',
+    role: 'Father of the Nation'
+  };
+};
+
+
+// TESTS:
+console.log('Running tests');
+try {
+  assert.equal(getNewUserInstance(), getNewUserInstance(), 'should be equal');
+  console.log('OK!');
+} catch(e) {
+  console.log('Tests failed', e.message);
+}
